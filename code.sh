5305544c1f6714065d4f02af49c26cbd6517d3ea
Manikprabhu1999 <mbjewargi@gmail.com>
2022-09-26 09:59:12 +0530

    day 1 and 2

---
+++ code.sh
@@ -0,0 +1,10 @@
+	a=4
+	b=7
+		if [ $a -gt $b ]
+
+		then
+		echo "value of a is greater than b"
+       else
+		echo "value of b is greater than a"
+
+	fi
---
+++ number.sh
@@ -0,0 +1,7 @@
+        for i=o
+	if[ $even -eq 0 ]
+	then
+		echo "even"
+	else
+		echo "odd"
+	fi
